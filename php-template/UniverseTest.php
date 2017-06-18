e9c6dd382671b572b695150ebff348a6b04856ca
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-17 21:12:00 -0700

    Fixed Reading File

--- php-template/UniverseTest.php
+++ php-template/UniverseTest.php
@@ -2,15 +2,26 @@
     include 'Universe.php';
     function test() {
         if (countAllStars([2,3]) ==  5 && countAllStars([9,-3]) == 6) {
-            if( strpos(file_get_contents("./Universe.php"),$_GET['array_sum']) !== false) {
+
+            $file = file_get_contents("./Universe.php");
+            if (strpos($file, "array_sum") !== false) {
                 echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏 * ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ");
                 echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ");
                 echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏 __.-._     ° . .　　　　.　☾ ° 　. *   ¸ .");
                 echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏  /'.-c    　   * ●  ¸.　　°     ° 　¸.    ");
                 echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏  |  /T      　　°     ° 　¸.     ¸ .　　  ");
-                echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏 _)_/LI");    
+                echo("TECHIO> message --channel Sucess! My personal Yoda, you are. 🙏 _)_/LI");
+            }
+            else
+            {
+                echo("TECHIO> message --channel Hint: Good Job! Try using array_sum");
+            }
+
+
+            if( strpos(file_get_contents("./Universe.php"),$_GET['array_sum']) !== false) {
+                    
             } else {
-                echo("TECHIO> message --channel Hint: Try using array_sum");
+                
             } 
         } else {
             echo("TECHIO> message --channel Oops! Try Again 🐞");
